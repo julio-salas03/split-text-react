bed0c91636784144c41a9202b5bf5a7c0bc70e1b
Julio Salas <84051776+julio-salas03@users.noreply.github.com>
2024-09-02 20:24:12 -0500

    add resizable component to test split text in storybook

--- stories/SplitText.stories.tsx
+++ stories/SplitText.stories.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { SplitText, SplitTextProps } from './../src/index';
+import { Resizable } from 'react-resizable/';
+import 'react-resizable/css/styles.css';
+
 const meta: Meta<typeof SplitText> = {
   component: SplitText,
 };
@@ -10,10 +13,27 @@ export default meta;
 type Story = StoryObj<typeof SplitText>;
 
 const Component = ({ children, ...rest }: SplitTextProps) => {
+  const [size, setSize] = React.useState({ width: 300, height: 400 });
   return (
-    <div>
-      <SplitText {...rest}>{children}</SplitText>
-    </div>
+    <>
+      <Resizable
+        height={size.height}
+        width={size.width}
+        onResize={(_, { size }) => setSize(size)}
+      >
+        <div
+          className="box"
+          style={{
+            width: size.width + 'px',
+            height: size.height + 'px',
+            border: 'solid red 4px',
+            padding: '10px',
+          }}
+        >
+          <SplitText {...rest}>{children}</SplitText>
+        </div>
+      </Resizable>
+    </>
   );
 };
 
